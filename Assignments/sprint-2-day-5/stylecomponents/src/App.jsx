835af640dd443f0e0ac26fb9fe01966012176d63
Kamlesh Jadhav <kamleshjadhav4393@gmail.com>
2022-03-26 21:53:17 +0530

    style-components

---
+++ Assignments/sprint-2-day-5/stylecomponents/src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+import { Button } from './Components/Button'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Button primary border="none" color='white'>
+        Primary Button
+      </Button>
+      <br />
+      <Button>
+        Default Button
+      </Button>
+      <br />
+      <Button border="dashed">Dash Button</Button>
+      <br />
+      <Button border="none">Text Button</Button>
+      <br />
+      <Button border="none" color='dodgerblue'>Link Button</Button>
+    </div>
+  )
+}
+
+export default App
